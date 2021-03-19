9e68f321f40279ee92c47fc3c4795c6d666c94a1
aikizoku <>
2021-03-19 10:39:17 +0900

    no message

--- main.go
+++ main.go
@@ -3,45 +3,33 @@ package main
 import (
 	"context"
 	"fmt"
-	"sync"
 
 	"cloud.google.com/go/pubsub"
 )
 
-var (
-	topic      *pubsub.Topic
-	messagesMu sync.Mutex
-	messages   []string
-)
-
-const maxMessages = 10
-
 func main() {
+	ctx := context.Background()
 	projectID := "develop-aikizoku"
 	subID := "pull-test-sub"
-	pullMsgs(projectID, subID)
+	pullMsgs(ctx, projectID, subID)
 }
 
-func pullMsgs(projectID string, subID string) {
-	ctx := context.Background()
+func pullMsgs(ctx context.Context, projectID string, subID string) {
+	// PubSubClientを作成
 	client, err := pubsub.NewClient(ctx, projectID)
 	if err != nil {
 		fmt.Printf("pubsub.NewClient: %v\n", err)
 		return
 	}
-	var mu sync.Mutex
-	received := 0
+
+	// Subscription(Pull型)を指定
 	sub := client.Subscription(subID)
-	cctx, cancel := context.WithCancel(ctx)
-	err = sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
-		mu.Lock()
-		defer mu.Unlock()
+
+	// レシーブ開始
+	err = sub.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
+		// 受け取ったら書き込む
 		fmt.Printf("Got message: %q\n", string(msg.Data))
 		msg.Ack()
-		received++
-		if received == 10 {
-			cancel()
-		}
 	})
 	if err != nil {
 		fmt.Printf("Receive: %v\n", err)
